aa9fbb36e96d4f2e05f609680abfd498ffc26c9f
Devon Bray <dev@esologic.com>
2026-01-10 22:01:06 -0500

    init

---
+++ gpu_box_benchmark/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for GPU Box Benchmark."""
+
+__author__ = """Devon Bray"""
+__email__ = "dev@esologic.com"
---
+++ test/test_gpu_box_benchmark.py
@@ -0,0 +1,8 @@
+"""Tests for `gpu_box_benchmark` package."""
+
+
+def test_main() -> None:
+    """
+    TODO: Test for the main entry point of gpu_box_benchmark
+    :return: None
+    """
